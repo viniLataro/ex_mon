a82647a4693b06a7c46bce7d2c12e3e0f6323b63
Vinicius Lataro <viniciusr.lataro@gmail.com>
2022-12-09 18:30:24 -0300

    Checking player moves

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,6 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
-  alias ExMon.Game.Status
+  alias ExMon.Game.{Actions, Status}
 
   @computer_name "Robotinik"
 
@@ -15,4 +15,9 @@ defmodule ExMon do
 
     Status.print_round_message()
   end
+
+  def make_move(move) do
+    Actions.fetch_move(move)
+  end
+  
 end
